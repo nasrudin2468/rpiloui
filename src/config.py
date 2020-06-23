b76b488a41117773defaf44caa34a81e34f337f2
nasrudin2468 <anony_mouz@web.de>
2020-06-23 09:37:47 +0200

    replace config object init function with class - requirement for pipability (multiprocessing -> pipe -> pickle)

--- src/config.py
+++ src/config.py
@@ -55,6 +55,102 @@ if __name__ == '__main__':
 ################################################################################
 # Functions
 
+class objgame:
+    def __init__(self):
+        # create new Parser and read file
+        config = configparser.ConfigParser()
+        config.read('rpiloui.cfg')
+
+        self.tiltmaxtime     = int(config['GAMESETTINGS']['tiltmaxtime'])
+        self.tiltrecovery    = float(config['GAMESETTINGS']['tiltrecovery'])
+        
+class objmux:
+    def __init__(self):
+        # create new Parser and read file
+        config = configparser.ConfigParser()
+        config.read('rpiloui.cfg')
+        
+        self.input      = [None] * 8
+        self.input[0] = int(config['PINMAPPING']['mux-0'])
+        self.input[1] = int(config['PINMAPPING']['mux-1'])
+        self.input[2] = int(config['PINMAPPING']['mux-2'])
+        self.input[3] = int(config['PINMAPPING']['mux-3'])
+        self.input[4] = int(config['PINMAPPING']['mux-4'])
+        self.input[5] = int(config['PINMAPPING']['mux-5'])
+        self.input[6] = int(config['PINMAPPING']['mux-6'])
+        self.input[7] = int(config['PINMAPPING']['mux-7'])
+
+class objmapinput:
+    def __init__(self):
+        # create new Parser and read file
+        config = configparser.ConfigParser()
+        config.read('rpiloui.cfg')
+        
+        self.coinsensor      = [None] * 8
+        self.coinsensor[0]   = int(config['MUXMAPPING']['coin0'])
+        self.coinsensor[1]   = int(config['MUXMAPPING']['coin1'])
+        self.coinsensor[2]   = int(config['MUXMAPPING']['coin2'])
+        self.coinsensor[3]   = int(config['MUXMAPPING']['coin3'])
+        self.coinsensor[4]   = int(config['MUXMAPPING']['coin4'])
+        self.coinsensor[5]   = int(config['MUXMAPPING']['coin5'])
+        self.coinsensor[6]   = int(config['MUXMAPPING']['coin6'])
+        self.coinsensor[7]   = int(config['MUXMAPPING']['coin7'])
+    
+        self.tiltsensor      = [None] * 8
+        self.tiltsensor[0]   = int(config['MUXMAPPING']['tilt0'])
+        self.tiltsensor[1]   = int(config['MUXMAPPING']['tilt1'])
+        self.tiltsensor[2]   = int(config['MUXMAPPING']['tilt2'])
+        self.tiltsensor[3]   = int(config['MUXMAPPING']['tilt3'])
+        self.tiltsensor[4]   = int(config['MUXMAPPING']['tilt4'])
+        self.tiltsensor[5]   = int(config['MUXMAPPING']['tilt5'])
+        self.tiltsensor[6]   = int(config['MUXMAPPING']['tilt6'])
+        self.tiltsensor[7]   = int(config['MUXMAPPING']['tilt7'])
+    
+        self.actionbutton    = [None] * 8
+        self.actionbutton[0] = int(config['MUXMAPPING']['action0'])
+        self.actionbutton[1] = int(config['MUXMAPPING']['action1'])
+        self.actionbutton[2] = int(config['MUXMAPPING']['action2'])
+        self.actionbutton[3] = int(config['MUXMAPPING']['action3'])
+        self.actionbutton[4] = int(config['MUXMAPPING']['action4'])
+        self.actionbutton[5] = int(config['MUXMAPPING']['action5'])
+        self.actionbutton[6] = int(config['MUXMAPPING']['action6'])
+        self.actionbutton[7] = int(config['MUXMAPPING']['action7']) 
+        
+class objconfig:
+    def __init__(self):
+        # create new Parser and read file
+        config = configparser.ConfigParser()
+        config.read('rpiloui.cfg')
+    
+        # Save values from file into given array
+        # MAIN
+        self.debug        = config['MAIN']['debug']
+    
+        #GAMESETTINGS
+        self.game                 = objgame()
+    
+        #CALIBRATION
+        self.maxduty      = int(config['CALIBRATION']['maxduty'])
+        self.pwmfreq      = int(config['CALIBRATION']['pwmfreq'])
+        self.dirdeathtime = int(config['CALIBRATION']['dirdeathtime'])
+    
+        #PINMAPPING
+        self.led_di       = int(config['PINMAPPING']['led-di'])
+        self.led_ci       = int(config['PINMAPPING']['led-ci'])
+    
+        self.motor_cw     = int(config['PINMAPPING']['motor-cw'])
+        self.motor_ccw    = int(config['PINMAPPING']['motor-ccw'])
+        self.mux_coin     = int(config['PINMAPPING']['mux-coin'])
+        self.mux_tilt     = int(config['PINMAPPING']['mux-tilt'])
+        self.mux_action   = int(config['PINMAPPING']['mux-action'])
+        
+        self.mux          = objmux()
+    
+        #MUXMAPPING
+        self.mapinput          = objmapinput()
+    
+        
+    
 # function:    read(objcfg)
 #     -     reads config file and saves variables into given array
 #     Input:    objcfg - name of object which will hold save data
